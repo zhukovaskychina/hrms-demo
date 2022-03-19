e78aa788321ef93416a3770754fa14ec2720c501
zhukovasky <zhukovasky@163.com>
2022-03-20 00:43:52 +0800

    初始化上传

--- hrms/hrms-rest/src/main/java/com/wiseoutbound/classloader/utils/DevOpsClassLoaderUtils.java
+++
@@ -1,9 +0,0 @@
-package com.wiseoutbound.classloader.utils;
-
-public class DevOpsClassLoaderUtils {
-
-
-    public static ClassLoader getClassLoader(String dir) {
-        return ClassLoaderHelper.getClassLoaderHelper(dir).getClassLoader();
-    }
-}
